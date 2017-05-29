323d037e7ad43a358635f44f911f278d448620c3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-29 13:04:32 -0700

    Working on removing extra characters that are non-alphabetical, but it is not working.

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,6 @@
 # put your code here.
 def count_word(file_name):
-    """given a text file, counts number of time a word appers in a text file and returns the word and number of times it appears"""
+    """given a text file, counts number of time a word appears in a text file and returns the word and number of times it appears"""
     # open_file = 
 
     count_of_words = {}
@@ -11,6 +11,16 @@ def count_word(file_name):
             line = line.strip()
             data = line.split()
             for word in data:
+            #     if (word + ","):
+            #         word = word.rstrip(",")
+            #     elif (word + ";"):
+            #         word = word.rstrip(";")
+            #     elif (word + "."):
+            #         word = word.rstrip(".")
+            #     elif ("'" + word):
+            #         word = word.lstrip("'")
+            #     elif isupper(word[0]):
+            #         word = word.lower()
                 count_of_words[word] = count_of_words.get(word, 0) + 1
 
     # open_file.close()
